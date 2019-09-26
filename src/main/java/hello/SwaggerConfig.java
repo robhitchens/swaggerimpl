f6f380d30d205bbd7c4a55f10073623ffd70a706
hitc6176 <rhitchens@wsfsbank.com>
2019-09-26 10:59:47 -0400

    added global 500 and 403 messages for api

--- src/main/java/hello/SwaggerConfig.java
+++ src/main/java/hello/SwaggerConfig.java
@@ -1,14 +1,20 @@
 package hello;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -25,7 +31,21 @@ public class SwaggerConfig {
         .apis(RequestHandlerSelectors.any())
         .paths(PathSelectors.any())
         .build()
-        .apiInfo(apiInfo());
+        .apiInfo(apiInfo())
+        .useDefaultResponseMessages(false)
+        .globalResponseMessage(RequestMethod.GET, List.of(
+                new ResponseMessageBuilder()
+                .code(500)
+                .message("500 message")
+                .responseModel(new ModelRef("Error"))
+                .build(),
+
+                new ResponseMessageBuilder()
+                .code( 403)
+                .message("Forbidden")
+                .build()
+        ))
+        ;
     }
 
     private ApiInfo apiInfo(){
