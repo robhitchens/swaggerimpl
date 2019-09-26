fc61b7d174739565d6b46ded68c077c225e25426
hitc6176 <rhitchens@wsfsbank.com>
2019-09-26 09:00:11 -0400

    added comment

--- src/main/java/hello/SwaggerConfig.java
+++ src/main/java/hello/SwaggerConfig.java
@@ -12,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
+    //NOTE Swagger UI is accessible at localhost:8080/swagger-ui.html
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
