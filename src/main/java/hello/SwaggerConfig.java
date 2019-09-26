f47e148726f5ae697ed500dbc049a921672625c5
hitc6176 <rhitchens@wsfsbank.com>
2019-09-26 09:11:19 -0400

    added api info

--- src/main/java/hello/SwaggerConfig.java
+++ src/main/java/hello/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package hello;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,8 +21,22 @@ public class SwaggerConfig {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
         .select()
+        //can also restrict documentation using RequestHandlerSelectors.basePackage("com.wsfsbank.package").
         .apis(RequestHandlerSelectors.any())
         .paths(PathSelectors.any())
-        .build();
+        .build()
+        .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfo(
+            "My REST Api",
+            "Some custom description of API",
+            "API TOS",
+            "Terms of service",
+            new Contact("Robert Hitchens", "myfoot.ur.ass", "rhitchens@wsfsbank.com"),
+            "License of API",
+            "API License URL",
+            Collections.emptyList());//Vender extensions.
     }
 }
