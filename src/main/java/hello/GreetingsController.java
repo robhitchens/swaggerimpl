a792474767e02cdc36854b111e5d4b8661ea7c49
hitc6176 <rhitchens@wsfsbank.com>
2019-09-30 08:51:43 -0400

    added ApiOperation annotation to greetings controller

--- src/main/java/hello/GreetingsController.java
+++ src/main/java/hello/GreetingsController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,11 @@ public class GreetingsController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @ApiOperation(
+            value="Returns a greeting",
+            notes = "Accepts name as query parameter for generating a greeting, defaults to world",
+            response = Greeting.class
+    )
     @RequestMapping(value = "/greeting", method = RequestMethod.GET)
     public Greeting greeting(@RequestParam(value="name", defaultValue = "World")String name){
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
