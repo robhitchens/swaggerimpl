ef18d010554d4ac46551ce00630c90e753cccfb1
hitc6176 <rhitchens@wsfsbank.com>
2019-09-30 08:46:24 -0400

    added dummy DTO

---
+++ src/main/java/hello/DummyDTO.java
@@ -0,0 +1,22 @@
+package hello;
+
+public class DummyDTO {
+    private String propertyA;
+    private String propertyB;
+
+    public String getPropertyA() {
+        return propertyA;
+    }
+
+    public void setPropertyA(String propertyA) {
+        this.propertyA = propertyA;
+    }
+
+    public String getPropertyB() {
+        return propertyB;
+    }
+
+    public void setPropertyB(String propertyB) {
+        this.propertyB = propertyB;
+    }
+}
