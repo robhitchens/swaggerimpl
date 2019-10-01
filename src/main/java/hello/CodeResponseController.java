1bb2a8c21a507eb196d3f54f99fa85bf3962ab18
hitc6176 <rhitchens@wsfsbank.com>
2019-10-01 09:48:03 -0400

    added controller with @ApiResponse examples

---
+++ src/main/java/hello/CodeResponseController.java
@@ -0,0 +1,69 @@
+package hello;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CodeResponseController {
+
+    @ApiOperation(
+            value = "Returns ResponseEntity of provided code",
+            notes = "Accepts a query parameter of type int and returns a ResponseEntity with the passed in code",
+            response = ResponseEntity.class
+    )
+    @ApiResponses({
+            @ApiResponse(code = 100, message = "Continue"),
+            @ApiResponse(code = 101, message = "Switching protocols"),
+            @ApiResponse(code = 200, message = "Success, OK!"),
+            @ApiResponse(code = 201, message = "Created"),
+            @ApiResponse(code = 202, message = "Accepted"),
+            @ApiResponse(code = 203, message = "Non-Authoritative Information"),
+            @ApiResponse(code = 204, message = "No Content"),
+            @ApiResponse(code = 205, message = "Reset Content"),
+            @ApiResponse(code = 206, message = "Partial Content"),
+            @ApiResponse(code = 300, message = "Multiple Choices"),
+            @ApiResponse(code = 301, message = "Moved Permanently"),
+            @ApiResponse(code = 302, message = "Found"),
+            @ApiResponse(code = 303, message = "See Other"),
+            @ApiResponse(code = 304, message = "Not Modified"),
+            @ApiResponse(code = 305, message = "Use Proxy"),
+            @ApiResponse(code = 306, message = "(Unused)"),
+            @ApiResponse(code = 307, message = "Temporary Redirect"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 402, message = "Payment Required"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 405, message = "Method Not Allowed"),
+            @ApiResponse(code = 406, message = "Not Acceptable"),
+            @ApiResponse(code = 407, message = "Proxy Authentication Required"),
+            @ApiResponse(code = 408, message = "Request Timeout"),
+            @ApiResponse(code = 409, message = "Conflict"),
+            @ApiResponse(code = 410, message = "Gone"),
+            @ApiResponse(code = 411, message = "Length Required"),
+            @ApiResponse(code = 412, message = "Precondition Failed"),
+            @ApiResponse(code = 413, message = "Request Entity Too Large"),
+            @ApiResponse(code = 414, message = "Request-URI Too Long"),
+            @ApiResponse(code = 415, message = "Unsupported Media Type"),
+            @ApiResponse(code = 416, message = "Requested Range Not Satisfiable"),
+            @ApiResponse(code = 417, message = "Expectation Failed"),
+            @ApiResponse(code = 418, message = "I'm a teapot"),
+            @ApiResponse(code = 500, message = "Internal Server Error"),
+            @ApiResponse(code = 501, message = "Not Implemented"),
+            @ApiResponse(code = 502, message = "Bad Gateway"),
+            @ApiResponse(code = 503, message = "Service Unavailable"),
+            @ApiResponse(code = 504, message = "Gateway Timeout"),
+            @ApiResponse(code = 505, message = "HTTP Version Not Supported")
+    })
+    @RequestMapping(value = "/code", method = RequestMethod.GET, produces = {"application/json"})
+    public ResponseEntity returnErrorCode(@RequestParam(value = "return", defaultValue = "418")Integer errorCode){
+        return ResponseEntity.status(errorCode).build();
+    }
+}
